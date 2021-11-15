3d85edb5ae6b545fa81ebfb21f3cb37c7e31ab07
“Luciana <lsouzabianchini@gmail.com>
2021-11-15 19:52:58 -0300

    Database seeding

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('LUCIANA DE SOUZA BIANCHINI', '38507143814', 1000.0, '1972-03-11', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('WELLYSON BIANCHINI', '32053723874', 2500.0, '1973-10-09', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('CLEIDE SOARES MORAIS DE SOUZA', '22448673829', 1550.0, '1947-06-23', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('SOPHIA DE SOUZA BIANCHINI', '16353141866', 5080.0, '2012-02-13', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('MARIA EDUARDA FERREIRA', '08301173858', 5080.0, '2001-04-26', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('MARIA APARECIDA MORAIS DE GODOY', '23003769898', 3870.0, '1963-12-01', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('IRINEU DE SOUZA', '06831374840', 6230.0, '1936-01-13', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('ISABELLA DE SOUZA BIANCHINI', '27859018861', 4315.0, '2005-06-11', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('JANAINA FERREIRA BIANCHINI', '36052132353', 5695.0, '1979-04-17', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('ADRIANA REGINA DE SOUZA SANTOS', '35527074840', 1987.0, '1977-12-31', 1);
